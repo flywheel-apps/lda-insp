5ae28609a67cc839697d580be71c3cde83a26c8f
Jennifer Schommer <jenniferschommer@invenshure.com>
2017-04-13 17:24:16 -0500

    Remove unnecessary file

--- run_the_gear.sh
+++
@@ -1,5 +0,0 @@
-#### bold commands
-ROOTDIR=/Users/jenniferschommer/test-data/lda-insp_lowres
-
-docker run -it -v $ROOTDIR/inspiration:/flywheel/v0/input/inspiration \
-    -v $ROOTDIR/output:/flywheel/v0/output flywheel/lda-insp
