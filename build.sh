8f6e80d4b3382253dab93a58d7c394024caeeee1
Jennifer Schommer <jenniferschommer@invenshure.com>
2017-04-17 16:28:08 -0500

    Remove build.sh script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Builds the brain-extraction container.
-# The container can be exported using the export.sh script
-GEAR=flywheel/lda-insp
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build --no-cache --tag $GEAR $DIR
